f26ada359b799fbaf18e6a5256526332d483dc1f
heitor57 <heitorwerneck@hotmail.com>
2020-10-27 21:19:24 -0300

    process init and free

--- src/Process.c
+++ src/Process.c
@@ -1,5 +1,21 @@
 #include "Process.h"
 #include <stdbool.h>
+#include <stdlib.h>
+Process* initProcess(){
+  Process* p = malloc(sizeof(Process));
+  p->cpu_usage=0;
+  p->state=Ready;
+  time(&p->init_time);
+  if (p==NULL){
+    exit(1);
+  }
+  return p;
+}
+
+void freeProcess(Process* p){
+  if(p!=NULL)
+    free(p);
+}
 
 bool equalsProcess(const Object o1,const Object o2){
   Process* p1 = (Process*)o1;
--- src/Process.h
+++ src/Process.h
@@ -2,6 +2,7 @@
 #define PROCESS_H
 #include <stdbool.h>
 #include "List.h"
+#include <time.h>
 
 typedef enum State State;
 enum State {Blocked, Executing, Ready};
@@ -9,11 +10,14 @@ typedef struct{
   int id, parent_id, var, *pc, priority;
   char** program;
   State state;
-  float init_time, cpu_usage;
+  time_t init_time;
+  double cpu_usage;
+  
 }Process;
 
 bool equalsProcess(const Object o1,const Object o2);
-/* initProcess */
+Process* initProcess();
+void freeProcess(Process* p);
 /* readInput */
 /* createProcess */
 /* processSwitch */
