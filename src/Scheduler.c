196b4dd52375abf44ac80477b24f503db6137c26
ThalesMrad <thalesmradl@gmail.com>
2020-10-30 17:26:26 -0300

    priority_scheduling

--- src/Scheduler.c
+++ src/Scheduler.c
@@ -2,3 +2,15 @@
 ProcessManager* round_robin(ProcessManager* pm){
 
 }
+
+int priorityScheduling(ProcessManager* pm){
+  
+  int higherpriority = 0;
+  for(int i=0; i<sizeList(pm->ready_processes);i++){
+      if(getByIndexList(pm->ready_processes,i)->priority > higherpriority)
+        higherpriority = getByIndexList(pm->ready_processes,i)->priority;
+  }
+  getByIndexList(pm->ready_processes,higherpriority)->priority -= 1; 
+  return higherpriority;
+  
+}
