88ee00aa32334c36a2ecfcbda3e7030dc4ab8ca1
heitor57 <heitorwerneck@hotmail.com>
2020-10-30 17:54:50 -0300

    priority scheduling fix

--- src/Scheduler.c
+++ src/Scheduler.c
@@ -10,14 +10,15 @@ int round_robin(ProcessManager* pm){
 }
 
 int priorityScheduling(ProcessManager* pm){
-  
-  insertAtEndList(pm->ready_processes,getArrayList(pm->pcb_table,pm->executing_process);) 
+  insertAtEndList(pm->ready_processes,
+                  &(((Process*)getArrayList(pm->pcb_table,pm->executing_process))->id)
+                  );
   int higherpriority = 0;
   for(int i=0; i<sizeList(pm->ready_processes);i++){
-      if(getByIndexList(pm->ready_processes,i)->priority > higherpriority)
-        higherpriority = getByIndexList(pm->ready_processes,i)->priority;
+    if(((Process*)getByIndexList(pm->ready_processes,i))->priority > higherpriority)
+      higherpriority = ((Process*)getByIndexList(pm->ready_processes,i))->priority;
   }
-  getByIndexList(pm->ready_processes,higherpriority)->priority -= 1; 
+  ((Process*)getByIndexList(pm->ready_processes,higherpriority))->priority -= 1; 
   pm->executing_process = higherpriority;
   return higherpriority;
 
