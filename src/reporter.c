aa512d99bf46217de7ca8ebacfee6c9baf30b1c4
Felipe <narulee@hotmail.com>
2020-10-30 22:30:17 -0300

    header table

--- src/reporter.c
+++ src/reporter.c
@@ -12,7 +12,8 @@ void printProcessList(ArrayList pcb_table,List* process_list) {
       pid = ((int*)node->object);
       process = (Process*)getArrayList(pcb_table, *pid);
       /* process = ((Process*)node->object); */
-      printf("%d\t%d\t%d\t%d\t%d\t%d\n", process->id, process->parent_id, 
+      printf("PID\tPPID\tPrioridade\tValor\tTempo início\tCPU usada até o momento\n");
+      printf("%d\t%d\t%d\t\t%d\t%d\t\t%d\n", process->id, process->parent_id, 
              process->priority, process->var, 
              process->init_time, process->cpu_usage);
       node = node->next;
@@ -23,17 +24,19 @@ void printState(ProcessManager* pm) {
 
   printf("****************************************************************\n");
   printf("Estado do sistema:\n");
-  printf("****************************************************************\\\\\n\n");
+  printf("****************************************************************\\\\n\n");
 
   printf("TEMPO ATUAL: %d\n", pm->time);
   printf("PROCESSO EXECUTANDO:\n");
+  printf("PID\tPPID\tPrioridade\tValor\tTempo início\tCPU usada até o momento\n");
   Process* executing_process = (Process*)getArrayList(pm->pcb_table, pm->executing_process);
-  printf("%d\t%d\t%d\t%d\t%d\t%d\n", executing_process->id, executing_process->parent_id, 
+  printf("%d\t%d\t%d\t\t%d\t%d\t\t%d\n", executing_process->id, executing_process->parent_id, 
          executing_process->priority, executing_process->var, 
          executing_process->init_time, executing_process->cpu_usage);
 
-  printf("BLOQUEADO:\n");
-  printf("Fila processos bloqueados:\n");
+//   printf("BLOQUEADO:\n");
+//   printf("Fila processos bloqueados:\n");
+  printf("PROCESSOS BLOQUEADOS:\n");
   printProcessList(pm->pcb_table,pm->blocked_processes);
 
   printf("PROCESSOS PRONTOS:\n");
